99eeace4caf3eeed429dacb6140f54c6e6bf81ba
matheus.dvaz <matheus.dvaz@TIT0545537W10-1>
2025-02-25 19:27:31 -0300

    Adicionar arquivos de projeto.

---
+++ Aula01/Aula01/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.WriteLine("Hello, \nWorld!");
+
+Console.Write("Matheus \nDamasceno\n");
+
+string nomeCompleto = "Matheus Damasceno Vaz";
+
+string firstName = "Matheus";
+
+string lastName = "Vaz";
+
+string surname = "Damasceno";
+
+Console.WriteLine("Meu nome completo é " + firstName + " " + surname + " " + lastName);
+
+int idade = 28;
+
+
+Console.WriteLine($"Meu nome completo é {firstName} {surname} {lastName} e a minha idade é {idade} anos");
+
+Console.WriteLine("Escreva seu nome");
+
+string nome = Console.ReadLine();
+
+Console.WriteLine($"Olá {nome}, boa noite");
+
