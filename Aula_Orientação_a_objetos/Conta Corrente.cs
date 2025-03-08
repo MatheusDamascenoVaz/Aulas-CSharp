aa3f7850f58f80ef2033537ad6e4c14821f450f7
MatheusDamascenoVaz <matheusdamascenovaz@gmail.com>
2025-03-07 22:02:04 -0300

    Inclusão de novo exercício de classe com a conta corrente

---
+++ Aula_Orientação_a_objetos/Conta Corrente.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula_Orientação_a_objetos
+{
+    internal class Conta_Corrente
+    {
+        public string Titular;
+        public int Contatitular;
+        public double Saldo;
+        private double Saque;
+        private double Deposito;
+
+
+    public Conta_Corrente(string titular, int contatitular, double saldo)
+        {
+            Titular = titular;
+            Contatitular = contatitular;
+            Saldo = saldo;
+        }
+
+        public double Depositar(double deposito)
+        {
+            return Saldo + deposito ;
+        }
+        public string Sacar(double saque)
+        {
+            if(Saldo < saque) 
+            {
+                return "Não foi possível realizar o seu saque por falta se saldo";
+            }
+            Saldo = Saldo - saque;
+            return $"O saque foi realizado com sucesso, o novo saldo é {Saldo}";
+             
+        }
+         public double ExibeSaldo()
+        {
+            return Saldo ;
+        }
+
+        public string GetAccountDetails()
+        {
+            return $"Titular: {Titular} Conta: {Contatitular} Saldo: {Saldo}";
+        }
+
+    }
+}
--- Aula_Orientação_a_objetos/Program.cs
+++ Aula_Orientação_a_objetos/Program.cs
@@ -1,6 +1,8 @@
 ﻿
 using Aula_Orientação_a_objetos;
 
+double operacao;
+
 Produto produto1 = new Produto("Lápis", 2.50, 500);
 Produto produto2 = new Produto("Paçoca", 10.50,250);
 Produto produto3 = new Produto("Chá Baer Matte", 7.99,221);
@@ -10,7 +12,32 @@ Console.WriteLine(produto2.GetDetailProduct());
 Console.WriteLine(produto3.GetDetailProduct());
 
 Retangulo retangulo1 = new Retangulo(10, 15);
-Console.WriteLine(retangulo1.Area());
-Console.WriteLine(retangulo1.Perimetro());
+Console.WriteLine($"Área: {retangulo1.Area()} cm²");
+Console.WriteLine($"Perímetro: {retangulo1.Perimetro()} cm");
+
+Conta_Corrente conta_Corrente1 = new Conta_Corrente("Joelson", 12345, 5000);
+Conta_Corrente conta_Corrente2 = new Conta_Corrente("Xauflauer", 54321, 800);
+
+Console.WriteLine(conta_Corrente1.GetAccountDetails());
+
+do
+{
+
+    Console.WriteLine("Digite a transação desejada \n1 -> Depósito \n2 -> Saque \n3 -> Consultar saldo");
+    operacao = double.Parse(Console.ReadLine());
+}
+while (operacao < 1 || operacao > 3);
+
+Console.WriteLine("Insira o valor da Transação");
+double valorTransacao = double.Parse(Console.ReadLine());
+
+if (operacao == 1) { Console.WriteLine(conta_Corrente1.Depositar(valorTransacao)); }
+if (operacao == 2) { Console.WriteLine(conta_Corrente1.Sacar(valorTransacao)); }
+
+
+
+
+
+
 
 
