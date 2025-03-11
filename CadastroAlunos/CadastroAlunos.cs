196b4b36d078a7d222a8d665d60eb95b07b72f23
MatheusDamascenoVaz <matheusdamascenovaz@gmail.com>
2025-03-10 22:14:24 -0300

    Inclusão de funcionamento básico da atividade e inclusão do objeto de cadastro dos alunos

---
+++ CadastroAlunos/CadastroAlunos.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CadastroAlunos
+{
+    internal class CadastroAlunos
+    {
+        public string Nome;
+        public int Matrícula;
+        public int AnoLetivo;
+       
+
+        public CadastroAlunos(string nome, int matrícula, int anoLetivo, string email, string telefoneDeEmergencia)
+        {
+            Nome = nome;
+            Matrícula = matrícula;
+            AnoLetivo = anoLetivo;
+            
+        }
+
+        public string GetDetailedInfo()
+        {
+            return $"Aluno: {Nome} \n Matrícula: {Matrícula} \nAno letivo: {AnoLetivo} \n";
+        }
+            
+    }
+}
---
+++ CadastroAlunos/Program.cs
@@ -0,0 +1,24 @@
+﻿
+using System.Data;
+
+int continuar;
+string alunos;
+Console.WriteLine("Insira o nome do aluno");
+alunos = Console.ReadLine();
+
+    Console.WriteLine("Deseja adicionar um novo aluno? \n1 -> SIM \n2 -> NÃO");
+    continuar = Convert.ToInt32(Console.ReadLine());
+do
+{
+    
+    Console.WriteLine("Digite o nome do novo aluno");
+    alunos = Console.ReadLine();
+    
+
+    Console.Clear();
+}
+
+while (continuar == 1);
+Console.WriteLine("FIM");
+
+
