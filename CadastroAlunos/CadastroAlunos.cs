5035fc06790535ecc406b121b470821b72fbd0c0
MatheusDamascenoVaz <matheusdamascenovaz@gmail.com>
2025-03-11 21:57:06 -0300

    Mesclagem  de funções básicas do program CS para a classe e objeto e validação do professor

--- CadastroAlunos/CadastroAlunos.cs
+++ CadastroAlunos/CadastroAlunos.cs
@@ -18,6 +18,37 @@ namespace CadastroAlunos
             Idade = idade;
             Curso = curso;
         }
+
+        public static List<Alunos> listaAlunos = new List<Alunos>();
+
+
+           public static void AdicionarAluno() {
+
+            Console.WriteLine("Digite o nome do aluno:");
+            string nome = Console.ReadLine();
+
+            Console.WriteLine("Digite a idade do aluno:");
+            int idade = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite o curso do aluno:");
+            string curso = Console.ReadLine();
+
+            Alunos aluno = new Alunos(nome, idade, curso);
+
+            listaAlunos.Add(aluno);
+        }
+
+        public static void VisualizarAlunos() {
+
+            foreach (var aluno in listaAlunos)
+            {
+                Console.WriteLine($"Nome: {aluno.Nome}\nIdade: {aluno.Idade}\nCurso: {aluno.Curso}");
+                Console.WriteLine();
+            }
+
+        }
+
+
     }
 }
 
--- CadastroAlunos/Program.cs
+++ CadastroAlunos/Program.cs
@@ -8,33 +8,19 @@ class Program
     static void Main(string[] args)
     {
 
-        List<Alunos>listaAlunos = new List<Alunos>();
 string repeticao;
 
 do
 {
-    Console.WriteLine("Digite o nome do aluno:");
-    string nome = Console.ReadLine();
-
-    Console.WriteLine("Digite a idade do aluno:");
-    int idade = int.Parse(Console.ReadLine());
-
-    Console.WriteLine("Digite o curso do aluno:");
-    string curso = Console.ReadLine();
-
-    Alunos aluno = new Alunos(nome, idade, curso);
-    listaAlunos.Add(aluno);
-
+    Alunos.AdicionarAluno();
+                
     Console.WriteLine("Deseja adicionar mais alunos? (s/n)");
     repeticao = Console.ReadLine().ToLower();
 } while (repeticao == "s");
 
 Console.WriteLine("\nAlunos cadastrados:");
-foreach (var aluno in listaAlunos)
-{
-    Console.WriteLine($"Nome: {aluno.Nome}\nIdade: {aluno.Idade}\nCurso: {aluno.Curso}");
-    Console.WriteLine();
-}
+
+        Alunos.VisualizarAlunos();
     }
 }
 
