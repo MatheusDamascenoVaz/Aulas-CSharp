e9cc99a11f72bbb31bd950aaf5c4e3d567a9e6d4
MatheusDamascenoVaz <matheusdamascenovaz@gmail.com>
2025-03-26 22:08:13 -0300

    Inclusão de atividade explicativa sobre classes relacionatas a MessageBox, Random Number e DateTime

---
+++ ClassesImportantes/ClassesImportantes/Form1.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace ClassesImportantes
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnMessageBox = new System.Windows.Forms.Button();
+            this.labelResult = new System.Windows.Forms.Label();
+            this.btnRandom = new System.Windows.Forms.Button();
+            this.btnDateTime = new System.Windows.Forms.Button();
+            this.labelResult2 = new System.Windows.Forms.Label();
+            this.labelResult3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnMessageBox
+            // 
+            this.btnMessageBox.Location = new System.Drawing.Point(37, 361);
+            this.btnMessageBox.Name = "btnMessageBox";
+            this.btnMessageBox.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.btnMessageBox.Size = new System.Drawing.Size(108, 63);
+            this.btnMessageBox.TabIndex = 0;
+            this.btnMessageBox.Text = "MessageBox";
+            this.btnMessageBox.UseVisualStyleBackColor = true;
+            this.btnMessageBox.Click += new System.EventHandler(this.btnMessageBox_Click);
+            // 
+            // labelResult
+            // 
+            this.labelResult.AutoSize = true;
+            this.labelResult.Font = new System.Drawing.Font("Comic Sans MS", 20.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelResult.Location = new System.Drawing.Point(30, 37);
+            this.labelResult.Name = "labelResult";
+            this.labelResult.Size = new System.Drawing.Size(0, 38);
+            this.labelResult.TabIndex = 1;
+            // 
+            // btnRandom
+            // 
+            this.btnRandom.Location = new System.Drawing.Point(174, 361);
+            this.btnRandom.Name = "btnRandom";
+            this.btnRandom.Size = new System.Drawing.Size(111, 63);
+            this.btnRandom.TabIndex = 2;
+            this.btnRandom.Text = "Random";
+            this.btnRandom.UseVisualStyleBackColor = true;
+            this.btnRandom.Click += new System.EventHandler(this.btnRandom_Click);
+            // 
+            // btnDateTime
+            // 
+            this.btnDateTime.Location = new System.Drawing.Point(320, 361);
+            this.btnDateTime.Name = "btnDateTime";
+            this.btnDateTime.Size = new System.Drawing.Size(112, 63);
+            this.btnDateTime.TabIndex = 3;
+            this.btnDateTime.Text = "Date Time";
+            this.btnDateTime.UseVisualStyleBackColor = true;
+            this.btnDateTime.Click += new System.EventHandler(this.btnDateTime_Click);
+            // 
+            // labelResult2
+            // 
+            this.labelResult2.AutoSize = true;
+            this.labelResult2.Font = new System.Drawing.Font("Comic Sans MS", 20.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelResult2.Location = new System.Drawing.Point(30, 86);
+            this.labelResult2.Name = "labelResult2";
+            this.labelResult2.Size = new System.Drawing.Size(0, 38);
+            this.labelResult2.TabIndex = 4;
+            // 
+            // labelResult3
+            // 
+            this.labelResult3.AutoSize = true;
+            this.labelResult3.Font = new System.Drawing.Font("Comic Sans MS", 20.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelResult3.Location = new System.Drawing.Point(30, 133);
+            this.labelResult3.Name = "labelResult3";
+            this.labelResult3.Size = new System.Drawing.Size(0, 38);
+            this.labelResult3.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.labelResult3);
+            this.Controls.Add(this.labelResult2);
+            this.Controls.Add(this.btnDateTime);
+            this.Controls.Add(this.btnRandom);
+            this.Controls.Add(this.labelResult);
+            this.Controls.Add(this.btnMessageBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnMessageBox;
+        private System.Windows.Forms.Label labelResult;
+        private System.Windows.Forms.Button btnRandom;
+        private System.Windows.Forms.Button btnDateTime;
+        private System.Windows.Forms.Label labelResult2;
+        private System.Windows.Forms.Label labelResult3;
+    }
+}
+
---
+++ ClassesImportantes/ClassesImportantes/Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ClassesImportantes
+{
+    public partial class Form1: Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        // //////////////////////////////////////////////////////////////////// USO DA MESSAGE BOX 
+        private void btnMessageBox_Click(object sender, EventArgs e)
+        {
+            //MessageBox.Show("Olá, sou um botão exibindo uma mensagem");
+            //MessageBox.Show("Olá, sou um botão exibindo uma mensagem","Imagine um aviso bem legal aqui");
+            //MessageBox.Show("Olá, sou um botão exibindo uma mensagem", "Imagine um aviso bem legal aqui2",MessageBoxButtons.YesNoCancel);
+            DialogResult resultadoJanela = MessageBox.Show("Olá, sou um botão exibindo uma mensagem", "Imagine um aviso bem legal aqui3", MessageBoxButtons.YesNoCancel, MessageBoxIcon.Warning,MessageBoxDefaultButton.Button2);
+            if (resultadoJanela == DialogResult.Yes)
+            {
+                labelResult.Text = "Você selecionou a opção sim";
+            }
+            else if (resultadoJanela == DialogResult.No)
+            {
+                labelResult.Text = "Você selecionou a opção não";
+            }
+            else
+            {
+                labelResult.Text = string.Empty;
+            }
+
+                labelResult2.Text = resultadoJanela.ToString();
+        }
+        // //////////////////////////////////////////////////////////////// CLASSE RANDOM PARA TRATAR NUMEROS ALEATORIOS
+        private void btnRandom_Click(object sender, EventArgs e)
+        {
+            Random numeroAleatorio = new Random(DateTime.Now.Millisecond);
+            labelResult.Text = numeroAleatorio.Next(50,9999).ToString();
+            labelResult2.Text = numeroAleatorio.NextDouble().ToString();
+        }
+
+        private void btnDateTime_Click(object sender, EventArgs e)
+        {
+            DateTime dateTime = new DateTime();
+
+            //labelResult.Text = DateTime.Now.ToString();
+            labelResult2.Text = DateTime.Now.ToLongDateString();
+            labelResult3.Text = DateTime.Now.ToShortDateString();
+            labelResult.Text = DateTime.Now.ToLongTimeString();
+
+
+        }
+    }
+}
