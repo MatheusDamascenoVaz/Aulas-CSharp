d1aca06a7a11c91a726283b8a681e78a95f88024
MatheusDamascenoVaz <matheusdamascenovaz@gmail.com>
2025-03-31 20:49:32 -0300

    Inclusão de projeto exemplo demonstrando as funções de delay e fechamento automático de janelas

---
+++ AulaLayouts/AulaLayouts/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using AulaLayouts.Screen.Home;
+
+namespace AulaLayouts
+{
+    static class Program
+    {
+        /// <summary>
+        /// Ponto de entrada principal para o aplicativo.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Home home = new Home();
+            SplashScreen splashScreen = new SplashScreen();
+
+            Application.Run(splashScreen);
+
+            if (splashScreen.IsDisposed)
+            {
+                Application.Run(home);
+            }
+        }
+    }
+}
---
+++ AulaLayouts/AulaLayouts/Screen/SplashScreen/SplashScreen.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AulaLayouts
+{
+    public partial class SplashScreen: Form
+    {
+        public SplashScreen()
+        {
+            InitializeComponent();
+        }
+
+        private void SplashScreen_Load(object sender, EventArgs e)
+        {
+            CloseSplashScreen();
+        }
+
+        private async void CloseSplashScreen()
+        {
+            await Task.Delay(3000);
+            this.Close();
+        }
+    }
+}
